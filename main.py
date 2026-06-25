d5a57d3ca31ddfbc3b76824851894c5d712fb498
yashikaadhikari <yashikaadhikari4@gmail.com>
2026-06-26 00:49:26 +0530

    add changes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# CUSTOMER CHURN PREDICTION SYSTEM
+# Customer Churn Prediction System
 
 import numpy as np
 import pandas as pd
@@ -29,86 +29,7 @@ np.random.seed(42)
 n = 3000
 
 
-data = {
 
-
-    "CustomerID":
-    range(1001,1001+n),
-
-
-    "Age":
-    np.random.randint(18,70,n),
-
-
-
-    "Gender":
-    np.random.choice(
-        ["Male","Female"],
-        n
-    ),
-
-
-
-    "SubscriptionType":
-    np.random.choice(
-        ["Basic","Premium","Enterprise"],
-        n,
-        p=[0.5,0.35,0.15]
-    ),
-
-
-
-    "MonthlyCharges":
-    np.random.randint(
-        200,5000,n
-    ),
-
-
-
-    "TenureMonths":
-    np.random.randint(
-        1,72,n
-    ),
-
-
-
-    "SupportTickets":
-    np.random.randint(
-        0,15,n
-    ),
-
-
-
-    "PaymentMethod":
-    np.random.choice(
-        [
-            "Credit Card",
-            "UPI",
-            "Net Banking",
-            "Debit Card"
-        ],
-        n
-    ),
-
-
-
-    "SatisfactionScore":
-    np.random.randint(
-        1,11,n
-    ),
-
-
-
-    "UsageHoursPerWeek":
-    np.random.randint(
-        1,50,n
-    )
-
-}
-
-
-
-df = pd.DataFrame(data)
 
 
 
